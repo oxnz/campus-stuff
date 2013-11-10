6aa99f3c663e444af4e59504757fefab37585711
oxnz <yunxinyi@gmail.com>
2013-11-10 15:14:57 +0800

    update NZLogger

--- rdpp/src/NZLogger.cpp
+++ rdpp/src/NZLogger.cpp
@@ -1,17 +1,13 @@
+/*
+ * File: NZLogger.cpp
+ * Description:  A simple Logger class
+ * Author: Oxnz
+ * Mail: yunxinyi@gmail.com
+ * Copying: Copyright (C) 2013, All rights reserved.
+ *
+ * Revision: -*-
+ * Last-update: 2013-11-10 22:10:12
+ */
 #include "NZLogger.h"
 
 NZ::LOG_LEVEL NZ::NZLogger::m_level = DEBUG;
-
-void NZ::NZLogger::setLogLevel(LOG_LEVEL level) {
-    NZ::NZLogger::m_level = level;
-}
-
-void NZ::NZLogger::log(const std::string& msg) {
-    log(m_level, msg);
-}
-
-void NZ::NZLogger::log(LOG_LEVEL level, const std::string& msg) {
-    if (level < m_level)
-        return;
-	std::clog << LOG_LEVEL_PREFIX[level] << msg << std::endl;
-}
--- rdpp/src/NZLogger.h
+++ rdpp/src/NZLogger.h
@@ -1,3 +1,13 @@
+/*
+ * File: NZLogger.h
+ * Description:  A simple Logger class
+ * Author: Oxnz
+ * Mail: yunxinyi@gmail.com
+ * Copying: Copyright (C) 2013, All rights reserved.
+ *
+ * Revision: -*-
+ * Last-update: 2013-11-10 22:10:12
+ */
 #pragma once
 
 #include <iostream>
@@ -21,14 +31,63 @@ namespace NZ {
 
     class NZLogger {
     public:
-        static void setLogLevel(LOG_LEVEL level);
-        static void log(const std::string& msg);
-        static void log(LOG_LEVEL level, const std::string& msg);
-		static inline std::ostream& log(LOG_LEVEL level = INFO) {
-			return std::clog << LOG_LEVEL_PREFIX[level];
+        static void setLogLevel(LOG_LEVEL level) {
+			NZ::NZLogger::m_level = level;
+		}
+		/*
+		static void setLogStyle(const char* prefix, bool time,
+				const char* suffix) {
+			m_prefix = string(prefix);
+			m_bTime = time;
+		}
+		*/
+		static inline void log(const char* s) {
+			log(m_level, s);
+		}
+		static inline void log(LOG_LEVEL level, const char* s) {
+			if (level < m_level)
+				return;
+			std::clog << LOG_LEVEL_PREFIX[level];
+			printf(s);
+			std::clog << std::endl;
+		}
+		template<typename... Args>
+        static inline void log(const char* fmt, const Args&... args) {
+			log(m_level, fmt, args...);
+		}
+		template<typename... Args>
+		static inline void log(LOG_LEVEL level,
+				const char* fmt,
+				const Args&... args) {
+			if (level < m_level)
+				return;
+			std::clog << LOG_LEVEL_PREFIX[level];
+			printf(fmt, args...);
+			std::clog << std::endl;
+		}
+		static inline void printf(const char* s) {
+			while (*s) {
+				if (*s == '%' && *++s != '%')
+					std::clog << "*** log error: invalid format string,"
+					   << "	missing arguments" << std::endl;
+				std::clog << *s++;
+			}
+		}
+		template<typename T, typename... Args>
+			static void printf(const char* fmt, const T& value,
+					const Args&... args) {
+			while (*fmt) {
+				if (*fmt == '%' && *++fmt != '%') {
+					std::clog << value;
+					return printf(++fmt, args...);
+				} else
+					std::clog << *fmt++;
+			}
+			std::clog << "*** log error: extra arguments provided to"
+				<< std::endl;
+			//throw std::runtime_error("extra arguments provided to log");
 		}
     private:
-		static std::ostream os;
         static LOG_LEVEL m_level;
     };
 }
