96b60382da0743634ba176c82159e93dcdb74f9e
oxnz <yunxinyi@gmail.com>
2013-11-13 19:47:45 +0800

    add colorful logger

--- rdpp/src/NZLogger.h
+++ rdpp/src/NZLogger.h
@@ -1,12 +1,44 @@
 /*
  * File: NZLogger.h
- * Description:  A simple Logger class
+ * Description:  A Colorful Logger
  * Author: Oxnz
  * Mail: yunxinyi@gmail.com
  * Copying: Copyright (C) 2013 Oxnz All rights reserved.
  *
  * Revision: -*-
  * Last-update: 2013-11-10 22:10:12
+ */
+
+/*
+ * ref:
+ *
+ * {attr}:
+ * 	0 Reset all attributes (return to normal mode)
+ * 	1 Bright (Usually turns on BOLD)
+ * 	2 Dim
+ * 	3 Underline
+ * 	4 Blink
+ * 	5 Reverse
+ * 	6 Hidden
+ * {fg}:
+ *  30 Black
+ *  31 Red
+ *  32 Green
+ *  33 Yellow
+ *  34 Blue
+ *  35 Magenta
+ *  36 Cyan
+ *  37 White
+ * {bg}
+ *  40 Black
+ *  41 Red
+ *  42 Green
+ *  43 Yellow
+ *  44 Blue
+ *  45 Magenta
+ *  46 Cyan
+ *  47 White
+ * format: "\x1B[{attr};{fg};{bg}m\x1B[0m"
  */
 #pragma once
 
@@ -22,15 +54,6 @@ namespace NZ {
 		NOTICE, // notice always the highest privor
     };
 
-	static const char* LOG_LEVEL_PREFIX[] = {
-		"DEBUG: ",
-		"INFO: ",
-		"WARNING: ",
-		"ERROR: ",
-		"FATAL ERROR: ",
-		"NOTICE: ",
-	};
-
     class NZLogger {
     public:
         static void setLogLevel(LOG_LEVEL level) {
@@ -42,17 +65,21 @@ namespace NZ {
 			m_prefix = string(prefix);
 			m_bTime = time;
 		}
-		*/
 		static inline void log(const char* s) {
 			log(m_level, s);
 		}
 		static inline void log(LOG_LEVEL level, const char* s) {
 			if (level < m_level)
 				return;
+			if (level >= ERROR)
+				printf("%s", "\x1B[31m");
 			std::clog << LOG_LEVEL_PREFIX[level];
 			printf(s);
+			if (level >= ERROR)
+				printf("%s", "\x1B[0m");
 			std::clog << std::endl;
 		}
+		*/
 		template<typename... Args>
         static inline void log(const char* fmt, const Args&... args) {
 			log(m_level, fmt, args...);
@@ -63,8 +90,32 @@ namespace NZ {
 				const Args&... args) {
 			if (level < m_level)
 				return;
-			std::clog << LOG_LEVEL_PREFIX[level];
+			switch (level) {
+				case DEBUG:	// Magenta
+					printf("%s", "\x1B[35mDEBUG: ");
+					break;
+				case INFO:	// Blue
+					printf("%s", "\x1B[34mINFO: ");
+					break;
+				case WARNING: // red
+					printf("%s", "\x1B[31mWARNING: ");
+					break;
+				case ERROR:	// bright red
+					printf("%s", "\x1B[1;31mERROR: ");
+					break;
+				case FATAL:	// bright white on red
+					printf("%s", "\x1B[1;37;41mFATAL ERROR: ");
+					break;
+				case NOTICE:	// bright Green
+					printf("%s", "\x1B[1;32mNOTICE: ");
+					break;
+				default:	// Blue
+					printf("%s", "\x1B[34mLOG: ");
+					break;
+			}
+			//std::clog << LOG_LEVEL_PREFIX[level];
 			printf(fmt, args...);
+			printf("%s", "\x1B[0m");
 			std::clog << std::endl;
 		}
 		static inline void printf(const char* s) {
