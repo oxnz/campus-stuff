7c06c135e541c268618b5410e82a3087d258604f
oxnz <yunxinyi@gmail.com>
2013-12-08 00:11:11 +0800

    update help & version

--- compiler-principles/src/main.cpp
+++ compiler-principles/src/main.cpp
@@ -2,22 +2,44 @@
 
 int help(int ecode) {
 	(ecode == 0 ? std::cout : std::cerr) <<
-		"Usage:" << std::endl <<
-		"microc a small c compiler" << std::endl <<
-		"microc [options] <sourcefile>" <<
-		"  Options:" <<
+		"Overview: " << "microc compiler\n" <<
+		"Usage" << "microc [options] <inputs>\n" <<
+		"  Options:\n" <<
+		"    -h   --help     show this message and exit\n" <<
+		"    -v   --version  show version info and exit" <<
 		std::endl;
 
 	return ecode;
 }
 
-using MICROCC::Microcc;
+const std::string VERSION("0.12");
+const std::string MAJOR("0.1");
+const std::string MINOR("2");
+const std::string PLATFORM("x86_64-apple-darwin13.0");
+const std::string COPYING("Copyright (C) 2013 Oxnz, All rights reserved.");
+const std::string EMAIL("yunxinyi@gmail.com");
 
-int main(int argc, char* argv[]) {
-	if (argc == 1)
+void version(bool verbose) {
+	std::cout << "microcc version " << VERSION << " " <<
+		"(" << MAJOR << MINOR << ")" <<
+		"\nTarget: " << PLATFORM <<
+		"\n" << COPYING <<
+		//"\nBug Report: " << EMAIL <<
+		std::endl;
+}
+
+int main(int argc, const char* argv[]) {
+	if (argc == 1 || std::string(argv[1]) == "-h" ||
+			std::string(argv[1]) == "--help")
 		return help(0);
-	Microcc cc;
-	cc.compile(argv[1]);
+	else if (argc == 2 && (std::string(argv[1]) == "-v" ||
+			std::string(argv[1]) == "--version")) {
+		version(true);
+		return 0;
+	} else {
+		MICROCC::Microcc cc;
+		cc.compile(argv[1]);
+	}
 
 	return 0;
 }
