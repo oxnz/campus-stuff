c13e7e1597e70083b2ed6a47c28bcd0bfdcd4e1b
oxnz <yunxinyi@gmail.com>
2014-04-22 16:54:27 +0800

    remote AMOXP

--- AMOXP/keventwatch.d
+++
@@ -1,6 +0,0 @@
-/* Watch entry into kevent() */
-syscall::kevent:entry
-/execname == "dirwatcher" || execname == "DirectoryServic"/
-{
-	printf("%s called kevent()", execname);
-}
--- AMOXP/sizeprinter.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(void) {
-	printf("sizeof(int *) is %zu\n", sizeof(int *));
-
-	return 0;
-}
